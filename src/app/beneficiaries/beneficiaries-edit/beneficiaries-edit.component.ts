b2d2271b82f5d727984ee335541f8af534444567
Sameer Al Harbi <smrhrbi@gmail.com>
2019-10-13 23:06:48 +0300

    reaxtive

--- src/app/beneficiaries/beneficiaries-edit/beneficiaries-edit.component.ts
+++ src/app/beneficiaries/beneficiaries-edit/beneficiaries-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-beneficiaries-edit',
@@ -8,23 +8,91 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class BeneficiariesEditComponent implements OnInit {
 
-  relations = ['Family', 'Frind']
+  relations = ['Family', 'Frind'];
   beneficiaryForm: FormGroup;
+  forbiddenNames = ['Rami', 'Samer'];
 
   constructor() { }
 
   ngOnInit() {
     this.beneficiaryForm = new FormGroup(
       {
-        'code': new FormControl(null, Validators.required),
-        'name': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email]),
-        'relation': new FormControl('Frind')
+          'code': new FormControl(null, Validators.required),
+          'personalData': new FormGroup({
+          'name': new FormControl(null, [Validators.required, this.forbiddenName.bind(this)]),
+          'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail),
+          'relation': new FormControl('Frind')
+        }),
+        hobbies: new FormArray([])
+      });
+
+    // this.beneficiaryForm.valueChanges.subscribe(
+    //     (value) => console.log(value)
+    //   );
+    this.beneficiaryForm.statusChanges.subscribe(
+        (status) => console.log(status)
+      );
+
+      // this.beneficiaryForm.setValue({
+      //   'code':'0000',
+      //   'personalData':{
+      //     'name': 'sameer',
+      //     'email':'smrhrbi@gmail.com',
+      //     'relation': 'Family'
+      //   },
+      //   'hobbies':[]
+      // });
+
+      this.beneficiaryForm.patchValue({
+        'code':'0000',
+        'personalData':{
+          'name': 'sameer',
+          'email':'smrhrbi@gmail.com',
+          'relation': 'Family'
+        },
+        'hobbies':[]
       });
   }
 
+  getControls() {
+    return (this.beneficiaryForm.get('hobbies') as FormArray).controls;
+  }
+
+  get controls() {
+    return (this.beneficiaryForm.get('hobbies') as FormArray).controls;
+  }
+
+
   onSubmit() {
     console.log(this.beneficiaryForm);
+    this.beneficiaryForm.reset();
   }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (this.beneficiaryForm.get('hobbies') as FormArray).push(control);
+  }
+
+  forbiddenName(control: FormControl): {[s: string], boolean } {
+    if (this.forbiddenNames.indexOf(control.value) !== -1) {
+      return {nameIsForbidden: true};
+    }
+    return null;
+  }
+
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({emailIsForbidden: true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+
+    return promise;
+  }
+
 
 }
